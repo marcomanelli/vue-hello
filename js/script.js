27a46b190a63b6508a8809e28a06c579d0ce24b6
marcomanelli <tronmsi@gmail.com>
2021-05-04 17:09:47 +0200

    a

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* Descrizione:
+ - Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ - Cambiare il colore del testo cliccando su dei bottoni
+Bonus:
+1. Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+2. Cambiare la classe del testo scrivendola dentro a un input ma il cambio deve avvenire solo quando viene premuto invio */
+const app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Messaggio in h1'
+    },
+    methods: {
+      testoBlu: function() {
+        this.classList.add('blu')
+      }
+    }
+  })
